3178435336c8cb48d937fa6de45658172e98d0e9
Chase Carbaugh <chasec1@umbc.edu>
2019-10-30 02:25:03 -0400

    Fixed syscall definitions in skip_list.c

--- skip_list/skip_list.c
+++ skip_list/skip_list.c
@@ -1,8 +1,8 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
-#include <linux/sys/types.h>
 
-SYSCALL_DEFINE2(mbx421_init, unsigned int, ptrs, unsigned int, prob){
+
+SYSCALL_DEFINE2(mbx421_init, unsigned int ptrs, unsigned int prob){
 
 return 0;
 }
@@ -12,7 +12,7 @@ SYSCALL_DEFINE0(mbx421_shutdown){
         return 0;
 }
 
-SYSCALL_DEFINE1(mbx421_create, unsigned long, id){
+SYSCALL_DEFINE1(mbx421_create, unsigned long id){
 
 return 0;
 }
@@ -27,27 +27,27 @@ SYSCALL_DEFINE1(mbx421_count, unsigned long id){
 return 0;
 }
 
-SYSCALL_DEFINE3(mbx421_send, unsigned long, id, const unsigned char __user, *msg, long, len){
+SYSCALL_DEFINE3(mbx421_send, unsigned long id, const unsigned char __user *msg, long len){
 
 return 0;
 }
 
-SYSCALL_DEFINE3(mbx421_recv, unsigned long, id, unsigned char __user, *msg, long, len){
+SYSCALL_DEFINE3(mbx421_recv, unsigned long id, unsigned char __user *msg, long len){
 
 return 0;
 }
 
-SYSCALL_DEFINE1(mbx421_length, unsigned long, id){
+SYSCALL_DEFINE1(mbx421_length, unsigned long id){
 
 return 0;
 }
 
-SYSCALL_DEFINE2(mbx421_acl_add, unsigned long, id, pid_t, process_id){
+SYSCALL_DEFINE2(mbx421_acl_add, unsigned long id, pid_t process_id){
 
 return 0;
 }
 
-SYSCALL_DEFINE2(mbx421_acl_remove, unsigned long, id, pid_t, process_id){
+SYSCALL_DEFINE2(mbx421_acl_remove, unsigned long id, pid_t process_id){
 
 return 0;
 }
