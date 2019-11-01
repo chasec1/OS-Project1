aaec9fd3d641b3b4d7c01554a871abb3b0a943c0
Chase Carbaugh <chasec1@umbc.edu>
2019-11-01 15:24:42 -0400

    Driver1.c setup

--- proj1tests/Driver1.c
+++ proj1tests/Driver1.c
@@ -3,3 +3,22 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+
+#define sys_mbx421_init 434
+#define sys_mbx421_shutdown 435
+#define sys_mbx421_create 436
+#define sys_mbx421_destroy 437
+#define sys_mbx421_count 438
+#define sys_mbx421_send 439
+#define sys_mbx421_recv 440
+#define sys_mbx421_length 441
+#define sys_mbx421_acl_add 442
+#define sys_mbx421_acl_remove 443
+#define sys_skip_list_display 444
+
+int main(){
+    return 0;
+}
